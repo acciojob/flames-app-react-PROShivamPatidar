4cee2d77b177baf2ada4ba26a0e1267fd4aa9d5b
Shivam Patidar <125869590+PROShivamPatidar@users.noreply.github.com>
2024-02-01 10:40:02 +0000

    one

---
+++ src/components/Relation.js
@@ -0,0 +1,70 @@
+import React, { Component, useState } from "react";
+
+const Relation =()=>{
+        const [firstName, setFirstName] = useState('');
+        const [secondName, setSecondName] = useState('');
+        const [relationshipStatus, setRelationshipStatus] = useState('');
+
+        const calculateRelationship = () => {
+            if (firstName === '' || secondName === '') {
+                setRelationshipStatus('Please Enter valid input');
+                return;
+            }
+
+            const commonLetters = new Set([...firstName].filter(char => secondName.includes(char)));
+
+            const remainingFirstName = [...firstName].filter(char => !commonLetters.has(char)).join('');
+            const remainingSecondName = [...secondName].filter(char => !commonLetters.has(char)).join('');
+
+            const sumOfLengths = (remainingFirstName.length + remainingSecondName.length) % 6;
+
+            switch (sumOfLengths) {
+                case 1:
+                    setRelationshipStatus('Friends');
+                    break;
+                case 2:
+                    setRelationshipStatus('Love');
+                    break;
+                case 3:
+                    setRelationshipStatus('Affection');
+                    break;
+                case 4:
+                    setRelationshipStatus('Marriage');
+                    break;
+                case 5:
+                    setRelationshipStatus('Enemy');
+                    break;
+                case 0:
+                    setRelationshipStatus('Siblings');
+                    break;
+                default:
+                    setRelationshipStatus('');
+            }
+        };
+
+        const clearInputs = () => {
+            setFirstName('');
+            setSecondName('');
+            setRelationshipStatus('');
+        };
+
+
+        return (
+            <div id="main">
+                {/* Do not remove the main div */}
+                <input type="text" placeholder="Enter first name" value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    data-testid="input1" />
+                <input type="text" placeholder="Enter second name" value={secondName}
+                    onChange={(e) => setSecondName(e.target.value)}
+                    data-testid="input2" />
+                <button onClick={calculateRelationship} data-testid="calculate_relationship">Calculate Relationship Future</button>
+                <button onClick={clearInputs} data-testid="clear">Clear</button>
+
+                <h1>{relationshipStatus}</h1>
+            </div>
+        )
+    }
+
+
+export default Relation;
